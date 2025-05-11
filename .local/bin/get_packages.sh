11b91d48d95400b70011822882ad3ffbe029861c
Josh Quinlan <josh@quinlan.cloud>
2025-05-11 18:07:41 +0100

    Update package list

--- .local/bin/get_packages.sh
+++ .local/bin/get_packages.sh
@@ -2,42 +2,44 @@
 
 echo "Installing cask packages..."
 brew install --cask \
-  librewolf \
-  bitwarden \
-  thunderbird \
-  ghostty \
-  discord \
-  microsoft-teams \
-  microsoft-office \
-  zoom \
-  loopback \
-  vlc \
-  ente-auth \
-  cryptomator \
-  nextcloud \
-  gpg-suite \
-  dropbox \
-  openaudible \
-  spotify \
-  clipy \
-  rectangle \
-  keepingyouawake \
-  hammerspoon \
-  steam \
-  minecraft \
-  keymapp \
-  hiddenbar
+    librewolf \
+    bitwarden \
+    thunderbird \
+    ghostty \
+    discord \
+    microsoft-teams \
+    microsoft-office \
+    zoom \
+    loopback \
+    vlc \
+    ente-auth \
+    cryptomator \
+    nextcloud \
+    gpg-suite \
+    dropbox \
+    openaudible \
+    spotify \
+    clipy \
+    rectangle \
+    keepingyouawake \
+    hammerspoon \
+    steam \
+    minecraft \
+    keymapp \
+    hiddenbar \
+    mullvadvpn
 
 echo "Installing non-cask packages..."
 brew install \
-  neovim \
-  mpv \
-  most \
-  lsd \
-  hugo \
-  cyberduck \
-  tlrc \
-  fastfetch
+    neovim \
+    mpv \
+    most \
+    lsd \
+    hugo \
+    cyberduck \
+    tlrc \
+    fastfetch \
+    mas
 
 echo "Cleaning up and optimising brew..."
 brew cleanup
