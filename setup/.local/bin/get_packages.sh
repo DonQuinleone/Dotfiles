2b692d0b91edbd19641be1462c5ac3c8b011a916
Josh Quinlan <josh@quinlan.cloud>
2025-05-11 01:36:27 +0100

    Create script to install packages with Brew

---
+++ setup/.local/bin/get_packages.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+brew install --cask librewolf thunderbird ghostty discord microsoft-teams microsoft-office zoom loopback vlc ente-auth cryptomator nextcloud gpg-suite dropbox openaudible spotify clipy hammerspoon
+brew install neovim mpv stow most lsd hugo cyberduck
+brew install --cask steam minecraft
